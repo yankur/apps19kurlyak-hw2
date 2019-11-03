280608c5d9db021127ece8c2c5faf51f7dc3f3ba
yankur <42523950+yankur@users.noreply.github.com>
2019-11-03 19:53:46 +0200

    Update StackTest.java

--- src/test/java/ua/edu/ucu/collections/StackTest.java
+++ src/test/java/ua/edu/ucu/collections/StackTest.java
@@ -1,12 +1,49 @@
 package ua.edu.ucu.collections;
 
 import org.junit.Test;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class StackTest {
-    
+
     @Test
-    public void testSomeMethod() {
+    public void testPush() {
+        Stack stack = new Stack();
+        Object[] arr = new Object[] {"test1", "test2", "test3"};
+        for (Object el : arr) {
+            stack.push(el);
+        }
+        System.out.println(Arrays.toString(stack.toArray()));
+        Object[] newArr = stack.toArray();
+        Object[] expArr = new Object[] {"test1", "test2", "test3"};
+        assertArrayEquals(newArr, expArr);
+    }
+
+    @Test
+    public void testPeek() {
+        Stack stack = new Stack();
+        Object[] arr = new Object[] {"test1", "test2", "test3"};
+        for (Object el : arr) {
+            stack.push(el);
+        }
+        Object peek = stack.peek();
+        Object expPeek = "test3";
+        assertEquals(expPeek, peek);
+    }
+
+    @Test
+    public void testPop() {
+        Stack stack = new Stack();
+        Object[] arr = new Object[] {"test1", "test2", "test3"};
+        for (Object el : arr) {
+            stack.push(el);
+        }
+        Object peek = stack.pop();
+        Object expPeek = "test3";
+        System.out.println(Arrays.toString(stack.toArray()));
+        assertEquals(expPeek, peek);
     }
     
 }
