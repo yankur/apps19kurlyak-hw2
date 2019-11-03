b29827ec9de4b2dcee5a4f007875b8a41463ddf2
yankur <42523950+yankur@users.noreply.github.com>
2019-11-03 19:34:13 +0200

    Update QueueTest.java

--- src/test/java/ua/edu/ucu/collections/QueueTest.java
+++ src/test/java/ua/edu/ucu/collections/QueueTest.java
@@ -1,12 +1,51 @@
 package ua.edu.ucu.collections;
 
 import org.junit.Test;
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+import ua.edu.ucu.collections.immutable.ImmutableList;
+
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class QueueTest {
     
     @Test
-    public void testSomeMethod() {
+    public void testEnqueue() {
+        Queue queue = new Queue();
+        Object[] arr = new Object[] {"test1", "test2", "test3"};
+        for (Object el : arr) {
+            queue.enqueue(el);
+        }
+        System.out.println(Arrays.toString(queue.toArray()));
+        Object[] newArr = queue.toArray();
+        Object[] expArr = new Object[] {"test1", "test2", "test3"};
+        assertArrayEquals(newArr, expArr);
     }
-    
+
+    @Test
+    public void testPeek() {
+        Queue queue = new Queue();
+        Object[] arr = new Object[] {"test1", "test2", "test3"};
+        for (Object el : arr) {
+            queue.enqueue(el);
+        }
+        Object peek = queue.peek();
+        Object expPeek = "test1";
+        assertEquals(expPeek, peek);
+    }
+
+    @Test
+    public void testDequeue() {
+        Queue queue = new Queue();
+        Object[] arr = new Object[] {"test1", "test2", "test3"};
+        for (Object el : arr) {
+            queue.enqueue(el);
+        }
+        Object peek = queue.dequeue();
+        Object expPeek = "test1";
+        System.out.println(Arrays.toString(queue.toArray()));
+        assertEquals(expPeek, peek);
+    }
+
 }
