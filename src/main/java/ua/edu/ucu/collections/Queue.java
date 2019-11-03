023522212204f30558772e9d185d50ddde25e76e
yankur <42523950+yankur@users.noreply.github.com>
2019-11-03 19:32:07 +0200

    Update Queue.java

--- src/main/java/ua/edu/ucu/collections/Queue.java
+++ src/main/java/ua/edu/ucu/collections/Queue.java
@@ -1,5 +1,35 @@
 package ua.edu.ucu.collections;
 
-public class Queue {
-    
+import ua.edu.ucu.collections.immutable.ImmutableLinkedList;
+
+public class Stack {
+    private ImmutableLinkedList stack;
+
+    public Stack() {
+        stack = new ImmutableLinkedList();
+    }
+
+    Object peek() {
+        if (stack.isEmpty()) {
+            return new IndexOutOfBoundsException();
+        }
+        return stack.getLast();
+    }
+
+    Object pop() {
+        if (stack.isEmpty()) {
+            return new IndexOutOfBoundsException();
+        }
+        Object res = stack.getLast();
+        stack = stack.removeLast();
+        return res;
+    }
+
+    void push(Object e) {
+        stack = stack.addLast(e);
+    }
+
+    Object[] toArray() {
+        return stack.toArray();
+    }
 }
